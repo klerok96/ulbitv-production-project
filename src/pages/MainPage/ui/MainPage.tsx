a69dfd0bdbe6ae6073a63aee650818d728d48093
Andrey K <andrey@kikimora.tech>
2022-10-26 22:49:56 +0300

    add jest

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,7 +3,11 @@ import { useTranslation } from 'react-i18next';
 const MainPage = () => {
   const { t } = useTranslation('main');
 
-  return <div>{t('title')}</div>;
+  return (
+    <div>
+      {t('title')}
+    </div>
+  );
 };
 
 export default MainPage;
