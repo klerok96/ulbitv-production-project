1ffd7e7de5b8ee555f5e1284b036d73df583f3b5
Andrey K <andrey@kikimora.tech>
2022-10-30 16:57:40 +0300

    add BundleAnalyzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import Dotenv from 'dotenv-webpack';
 import { BuildOptions } from './types/config';
 
@@ -26,5 +27,10 @@ export function buildPlugins({
       'process.env.IS_DEV': isDev,
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin(
+      {
+        openAnalyzer: false,
+      },
+    ),
   ];
 }
